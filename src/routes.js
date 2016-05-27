766b66f09b17cb0835437761a9547f5bcf27ee5a
cody <wangkedi.wang@gmail.com>
2016-05-27 17:42:17 +0800

    finish front structure

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 import api from './api/routes';
-
+import client from './client/routes';
 const router = express.Router();
 
 router.use('/api', api);
-
+router.use('/client', client);
 
 export default router;
